1a130dbd1c999d4c467677deaadd2981e2aae8aa
Aryan Sankar <145859525+ironfist-15@users.noreply.github.com>
2025-12-15 00:29:09 +0530

    Third commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Redirect from "./Redirects.jsx";
 
-function Router() {
-  const path = window.location.pathname;
 
-  if (path !== "/") {
-    return <Redirect />;
-  }
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  return <App />;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Router />);
